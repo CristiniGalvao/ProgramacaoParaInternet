524696ec466431924e07ce670762257d8a2c1f9b
Gabriella Cristini <126834537+CristiniGalvao@users.noreply.github.com>
2024-05-13 22:27:26 -0300

    Add

--- Clinica/src/main/java/br/unipar/programacaointernet/clinica/controller/UsuarioWebController.java
+++ Clinica/src/main/java/br/unipar/programacaointernet/clinica/controller/UsuarioWebController.java
@@ -5,6 +5,7 @@ import br.unipar.programacaointernet.clinica.service.UsuarioService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -21,5 +22,10 @@ public class UsuarioWebController {
         List<Usuario> usuarios = usuarioService.getAll();
         model.addAttribute("usuarios",usuarios);
         return "usuarios";
+    }
+    @PostMapping(path = "/usuarios/save")
+    public String saveUsuarios(Usuario usuario){
+        usuarioService.save(usuario);
+        return "redirect:/usuarios";
     }
 }
